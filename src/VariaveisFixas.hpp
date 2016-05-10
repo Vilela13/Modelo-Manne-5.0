dcc6ad9c1198e9b071b67f11ba30571c6a882516
mateus.vilela <mateusvilelasouza@gmail.com>
2016-05-10 09:39:05 -0300

    3.09.01 - Tirei variavis desnecessarias do Head (.hpp) VariaveisFixas.

--- src/VariaveisFixas.hpp
+++ src/VariaveisFixas.hpp
@@ -9,14 +9,6 @@
 #define VARIAVEISFIXAS_HPP_
 
 
-// As unidades do problema são km e horas.
-
-#define NumeroDePlantasVariaveisFixas 2
-#define NumeroDeEntregasVariaveisFixas 4
-#define NumeroDeVeiculosVariaveisFixas 5
-#define VelocidadeVariaveisFixas 20
-#define CapacidadeCaminhao 10
-#define TempoDeVidaConcretoVariaveisFixas 1.5
 
 #define HorarioFuncionamentoEmpresaQuePodeAdiantar 0
 #define HorarioFuncionamentoEmpresaQuePodePostergar 24
