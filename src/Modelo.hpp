5129e09d9d12b47346308aba07dcaccc1d8abcf7
mateus.vilela <mateusvilelasouza@gmail.com>
2016-06-15 12:09:41 -0300

    3.14 - Mudei os pesos da penalidade caso se ultrapasse as janelas de tempo. Era 4000 e passou a 100000000.

--- src/Modelo.hpp
+++ src/Modelo.hpp
@@ -920,7 +920,7 @@ void No::CalculaPenalidadeDesrespeitoJanelaDeTempoEmpresa(int TipoPenalidade, in
 
 	if( TipoPenalidade == 2){
 		for( int e = 0; e < NE; e++){
-			PenalidadeDesrespeitoJanelaDeTempoEmpresa[e] = 10000;
+			PenalidadeDesrespeitoJanelaDeTempoEmpresa[e] = 100000000;
 			if( Escreve == 1){
 				cout << " Penalidade construcao[" << e << "] = " << PenalidadeDesrespeitoJanelaDeTempoEmpresa[e] << endl;
 			}
@@ -953,7 +953,7 @@ void No:: CalculaPenalidadeDesrespeitoJanelaDeTempoPlanta(int TipoPenalidade, in
 
 	if( TipoPenalidade == 2){
 		for( int p = 0; p < NP; p++){
-			PenalidadeDesrespeitoJanelaDeTempoPlanta[p] = 10000;
+			PenalidadeDesrespeitoJanelaDeTempoPlanta[p] = 100000000;
 			if( Escreve == 1){
 				cout << " Penalidade planta[" << p << "] = " << PenalidadeDesrespeitoJanelaDeTempoPlanta[p] << endl;
 			}
